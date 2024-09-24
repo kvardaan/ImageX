0f4fd032f9e707e3aa5f3a261b1472ba34cab042
kvardaan <vardaankhattar8907@gmail.com>
2024-09-24 21:25:21 +0530

    Added: Main Application

---
+++ apps/user-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<Button variant="link">Hi</Button>
+		</div>
+	);
+}
