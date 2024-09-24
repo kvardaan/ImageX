7aef7919de783e0be5d0dd62eab95be9db801496
kvardaan <vardaankhattar8907@gmail.com>
2024-09-24 21:31:57 +0530

    Added: Shadcn Config to Main Application

--- apps/user-app/src/app/page.tsx
+++ apps/user-app/src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@repo/ui/button";
-
 export default function Home() {
-	return (
-		<div className="flex justify-center items-center h-screen">
-			<Button variant="link">Hi</Button>
-		</div>
-	);
+	return <div className="flex justify-center items-center h-screen">ImageX</div>;
 }
