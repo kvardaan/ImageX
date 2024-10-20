4f6cd8ab5f5276616232b312bf05c15945199424
kvardaan <vardaankhattar8907@gmail.com>
2024-10-20 09:00:00 +0530

    Fix: Run-time Errors

--- src/lib/data/account.ts
+++ src/lib/data/account.ts
@@ -1,12 +1,11 @@
 import prisma from "@/lib/clients/prisma"
-import { Account } from "@/lib/types/account"
 
 /**
  * Finds the user's account from the DB using Id
  */
 export const getAccountByUserId = async (
   userId: string
-): Promise<Account | null> => {
+) => {
   try {
     const account = await prisma.account.findFirst({
       where: { userId },
